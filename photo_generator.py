7070838d76a8c4dcc56c881dc72e4a66a8a48daa
wrknatul <wrk.natul@yandex.ru>
2024-10-19 21:45:31 +0300

    renaming paths

--- photo_generator.py
+++ photo_generator.py
@@ -5,4 +5,4 @@ for i in range(100):
     for j in range(100):
         if (i - 50) ** 2 + (j - 50) ** 2 <= 26**2:
             photo[i][j] = 255
-cv2.imwrite("data/goal_photo.jpg", photo) 
+cv2.imwrite("kaggle/working/projectScanner/data/goal_photo.jpg", photo) 
