a8f79320f49892b0bafa945706e1b6f50f8e9f42
Лутан Максим Олегович <molutan@RM70154.local>
2024-11-09 10:25:57 +0300

    adding path into photo generator

--- photo_generator.py
+++ photo_generator.py
@@ -1,8 +1,16 @@
 import cv2
 import numpy as np
+import sys
+
 photo = np.zeros((100, 100))
 for i in range(100):
     for j in range(100):
         if (i - 50) ** 2 + (j - 50) ** 2 <= 26**2:
             photo[i][j] = 255
-cv2.imwrite("data/goal_photo.jpg", photo) 
+
+if len(sys.argv) != 2:
+    print("Usage: python photo_generator.py <output_path>")
+    sys.exit(1)
+
+output_path = sys.argv[1]
+cv2.imwrite(output_path, photo) 
